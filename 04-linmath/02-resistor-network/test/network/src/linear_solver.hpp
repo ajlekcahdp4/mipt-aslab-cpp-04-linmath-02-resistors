c9ebbec65aeb266c18eaee23adfc1ff44d658322
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-10-17 16:05:48 +0300

    start linear solver

--- 04-linmath/02-resistor-network/test/network/src/linear_solver.hpp
+++ 04-linmath/02-resistor-network/test/network/src/linear_solver.hpp
@@ -1,8 +1,35 @@
+#pragma once
+
 #include "matrix.hpp"
 
-#pragma once
+#include <range/v3/action.hpp>
+#include <range/v3/algorithm.hpp>
+#include <range/v3/view.hpp>
 
 namespace circuits {
 using matrix_f = throttle::linmath::matrix<float>;
 using matrix_d = throttle::linmath::matrix<double>;
+
+matrix_d nonsingular_solver(matrix_d &&xtnd_matrix) {
+  auto cols = xtnd_matrix.cols();
+  auto rows = xtnd_matrix.rows();
+  xtnd_matrix.convert_to_row_echelon();
+  matrix_d res{rows, 1};
+  for (unsigned i = 0; i < rows; i++)
+    res[i][0] = xtnd_matrix[i][cols - 1];
+  return res;
+}
+
+matrix_d linear_solver(const matrix_d &coefs, const matrix_d &col) {
+  auto rows = coefs.rows();
+  auto cols = coefs.cols() + 1;
+
+  matrix_d xtnd_matrix{rows, cols};
+  for (unsigned i = 0; i < rows; i++) {
+    auto concated = ranges::views::concat(coefs[i], col[i]);
+    ranges::copy(concated, xtnd_matrix.begin());
+  }
+  return nonsingular_solver(std::move(xtnd_matrix));
+}
+
 } // namespace circuits
