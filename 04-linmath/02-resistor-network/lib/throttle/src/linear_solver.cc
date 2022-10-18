dee9efc92389860ae0c324571db3244c126fc1e6
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-10-18 09:25:33 +0300

    handle dependent row case in linear solver

--- 04-linmath/02-resistor-network/lib/throttle/src/linear_solver.cc
+++ 04-linmath/02-resistor-network/lib/throttle/src/linear_solver.cc
@@ -12,15 +12,15 @@ namespace throttle {
 matrix_d nonsingular_solver(matrix_d &&xtnd_matrix) {
   auto cols = xtnd_matrix.cols();
   auto rows = xtnd_matrix.rows();
-
   xtnd_matrix.convert_to_row_echelon();
 
-  matrix_d res{rows, 1};
-  for (matrix_d::size_type i = 0; i < rows; i++) {
-    if (throttle::is_roughly_equal(xtnd_matrix[i][i], 0.0)) throw std::runtime_error("Singular matrix provided");
+  matrix_d res{cols - 1, 1};
+  for (matrix_d::size_type i = 0; i < cols - 1; i++) {
+    if (is_roughly_equal(xtnd_matrix[i][i], 0.0)) throw std::runtime_error("Singular matrix provided");
     res[i][0] = xtnd_matrix[i][cols - 1] / xtnd_matrix[i][i];
   }
-
+  for (matrix_d::size_type i = cols - 1; i < rows; i++)
+    if (!is_roughly_equal(xtnd_matrix[i][cols - 1], 0.0)) throw std::runtime_error("Singular matrix provided");
   return res;
 }
 
@@ -28,7 +28,7 @@ matrix_d nonsingular_solver(const matrix_d &coefs, const matrix_d &col) {
   auto rows = coefs.rows();
   auto cols = coefs.cols() + 1;
 
-  if (!coefs.square()) throw std::runtime_error("Non-square matrix provided");
+  if (rows < cols - 1) throw std::runtime_error("System has an infinite number of solutions");
   throttle::linmath::contiguous_matrix<double> xtnd_matrix{rows, cols};
 
   for (matrix_d::size_type i = 0; i < rows; i++) {
--- 04-linmath/02-resistor-network/lib/throttle/test/test_linear_solver.cc
+++ 04-linmath/02-resistor-network/lib/throttle/test/test_linear_solver.cc
@@ -25,3 +25,26 @@ TEST(test_linear_solver, test_2) {
   auto res = throttle::nonsingular_solver(coefs, col);
   EXPECT_EQ(res, sol);
 }
+
+TEST(test_linear_solver, dependent_system_1) {
+  throttle::matrix_d coefs{3, 2, {-1, 2, 2, 3, 1, -2}};
+  throttle::matrix_d col{3, 1, {0, 0, 0}};
+  throttle::matrix_d sol{2, 1, {0, 0}};
+  auto               res = throttle::nonsingular_solver(coefs, col);
+  EXPECT_EQ(res, sol);
+}
+
+TEST(test_linear_solver, dependent_system_2) {
+  throttle::matrix_d coefs{4, 3, {1, 1, 1, 0, -4, -10, 0, 2, 5, 2, 5, -1}};
+  throttle::matrix_d col{4, 1, {6, 8, -4, 27}};
+  throttle::matrix_d sol{3, 1, {5, 3, -2}};
+
+  auto res = throttle::nonsingular_solver(coefs, col);
+  EXPECT_EQ(res, sol);
+}
+
+TEST(test_linear_solver, singular_system) {
+  throttle::matrix_d coefs{4, 3, {1, 1, 1, 0, 5, -10, 0, 2, 5, 2, 5, -1}};
+  throttle::matrix_d col{4, 1, {6, 8, -4, 27}};
+  EXPECT_THROW(throttle::nonsingular_solver(coefs, col), std::runtime_error);
+}
