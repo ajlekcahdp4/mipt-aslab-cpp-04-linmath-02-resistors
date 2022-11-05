1b27f12043c90694c4d3bfb38b869508e63d39c0
Sergei Zimmerman <serjtsimmerman@gmail.com>
2022-11-05 17:46:40 +0300

    Change constexpr to const for compatability

--- 04-linmath/02-resistor-network/test/network/src/network.cc
+++ 04-linmath/02-resistor-network/test/network/src/network.cc
@@ -68,17 +68,17 @@ namespace circuit_parser {
 struct rule_d : error_handler {};
 struct rule_u : error_handler {};
 
-constexpr x3::rule<rule_d, double>   double_named = {"double"};
-constexpr x3::rule<rule_u, unsigned> unsigned_named = {"unsigned"};
+const x3::rule<rule_d, double>   double_named = {"double"};
+const x3::rule<rule_u, unsigned> unsigned_named = {"unsigned"};
 
-constexpr auto double_named_def = x3::real_parser<double>{};
-constexpr auto unsigned_named_def = x3::int_parser<unsigned>{};
+const auto double_named_def = x3::real_parser<double>{};
+const auto unsigned_named_def = x3::int_parser<unsigned>{};
 
 BOOST_SPIRIT_DEFINE(double_named, unsigned_named);
 
 struct edge_class;
 
-constexpr x3::rule<edge_class, circuit_parser::graph::network_edge> const edge = "edge";
+const x3::rule<edge_class, circuit_parser::graph::network_edge> const edge = "edge";
 
 const auto edge_def = unsigned_named > '-' > '-' > unsigned_named > ',' > double_named > ';' >> -(double_named >> 'V');
 
