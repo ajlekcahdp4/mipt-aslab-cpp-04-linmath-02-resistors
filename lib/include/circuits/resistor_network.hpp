0c62f32a891881bb58badcc6911b4f43cf78df19
Sergei Zimmerman <serjtsimmerman@gmail.com>
2023-01-23 23:13:14 +0300

    Split connected network solve function using a helper solver class

--- lib/include/circuits/resistor_network.hpp
+++ lib/include/circuits/resistor_network.hpp
@@ -60,52 +60,40 @@ private:
   circuit_graph_type              m_graph;
   std::vector<short_circuit_edge> m_short_circuits;
 
-public:
-  // The graph passed to this constructor must consist of only one connected components. This requirement is not
-  // validated in any way whatsoever. This class is not supposed to be used by the end user.
-  connected_resistor_network(circuit_graph_type graph) : m_graph{graph} {
-    for (const auto &v : m_graph) {
-      for (const auto &p : v.second) {
-        auto first = v.first, second = p.first;
-        auto [res, emf] = p.second;
-        if (throttle::is_roughly_equal(res, 0.0)) m_short_circuits.push_back({first, second, emf});
-      }
-    }
-  }
-
-  circuit_graph_type graph() const { return m_graph; }
-
-  solution solve() const {
-    if (m_graph.empty()) return {}; // If the network is empty, then there's nothing to do
-
-    const auto vertices = m_graph.vertices();
-    const auto num_short_circuits = m_short_circuits.size() / 2; // Divide by 2 because there are symmetric pairs
-
+  struct connected_resistor_network_solver {
     using system_type = linmath::linear_equation_system<double>;
     using equation_type = typename system_type::equation_type;
     using mapped_pair = std::pair<unsigned, unsigned>;
+
+    const connected_resistor_network &network;
 
     std::unordered_map<T, unsigned> id_map;         // Maps identifier from input to 0, 1, ...
     std::unordered_map<unsigned, T> inverse_id_map; // Maps 0, 1, ... to the input identifiers
     std::unordered_map<mapped_pair, std::pair<unsigned, double>, boost::hash<mapped_pair>>
         short_circuit_current_map; // Maps pairs of input indexes to current variable
+    const typename circuit_graph_type::size_type vertices, num_short_circuits;
+    unsigned                                     zero_potential_mapped_id;
 
-    for (auto j = 0; const auto &v : m_graph) {
-      id_map[v.first] = j;
-      inverse_id_map[j++] = v.first;
+    connected_resistor_network_solver(const connected_resistor_network &circuit)
+        : network{circuit}, vertices{network.m_graph.vertices()}, num_short_circuits{network.m_short_circuits.size() /
+                                                                                     2} {
+      for (auto j = 0; const auto &v : network.m_graph) {
+        id_map[v.first] = j;
+        inverse_id_map[j++] = v.first;
+      }
+
+      for (auto j = vertices; const auto &v : network.m_short_circuits) {
+        const auto  mapped_first = id_map.at(v.second), mapped_second = id_map.at(v.first);
+        mapped_pair canonical_pair = {mapped_first < mapped_second ? mapped_pair{mapped_first, mapped_second}
+                                                                   : mapped_pair{mapped_second, mapped_first}};
+        if (short_circuit_current_map.contains(canonical_pair)) continue;
+        short_circuit_current_map.insert({canonical_pair, {j++, v.emf}});
+      }
+
+      zero_potential_mapped_id = id_map.at(network.m_graph.begin()->first);
     }
 
-    for (auto j = vertices; const auto &v : m_short_circuits) {
-      const auto  mapped_first = id_map.at(v.second), mapped_second = id_map.at(v.first);
-      mapped_pair canonical_pair = {mapped_first < mapped_second ? mapped_pair{mapped_first, mapped_second}
-                                                                 : mapped_pair{mapped_second, mapped_first}};
-      if (short_circuit_current_map.contains(canonical_pair)) continue;
-      short_circuit_current_map.insert({canonical_pair, {j++, v.emf}});
-    }
-
-    const auto zero_potential_mapped_id = id_map.at(m_graph.begin()->first);
-
-    const auto make_system = [&]() {
+    auto make_system() const {
       const auto variables = vertices + num_short_circuits;
 
       // This is the linear system that we are constructing.
@@ -116,7 +104,7 @@ public:
       equation.reset();
 
       // Step 1. Iterate over all points and use vertex current method to construct a linear equation.
-      for (auto start = std::next(m_graph.begin()), finish = m_graph.end(); start != finish; ++start) {
+      for (auto start = std::next(network.m_graph.begin()), finish = network.m_graph.end(); start != finish; ++start) {
         const auto &v = *start;
 
         const auto &[current_id, adj_map] = v;
@@ -165,46 +153,72 @@ public:
       }
 
       return system;
-    };
-
-    // Solve the linear system of equations to find unkown potentials and currents.
-    auto system = make_system();
-
-    auto res = system.solve();
-    if (!res) throw circuit_error{"The circuit is undefined. Possible infinite current loop"};
-    auto unknowns = system.solve().value();
-
-    // Fill base node potential with zero.
-    auto result_potentials = solution_potentials{};
-    for (const auto &v : m_graph) {
-      const auto id = v.first;
-      const auto mapped = id_map[id];
-      result_potentials[id] = unknowns[mapped][0];
     }
 
-    // Fill unkown currents that were found as a part of linear system of m_equations.
-    auto result_currents = solution_currents{};
+    solution solve() const {
+      if (network.m_graph.empty()) return solution{}; // If the network is empty, then there's nothing to do
 
-    for (const auto &v : short_circuit_current_map) {
-      const auto fwd_current = unknowns[v.second.first][0];
-      const auto first_original_id = inverse_id_map[v.first.first];
-      const auto second_original_id = inverse_id_map[v.first.second];
-      result_currents[first_original_id][second_original_id] = fwd_current;
-      result_currents[second_original_id][first_original_id] = -fwd_current;
+      // Solve the linear system of equations to find unkown potentials and currents.
+      auto system = make_system();
+
+      auto res = system.solve();
+      if (!res) throw circuit_error{"The circuit is undefined. Possible infinite current loop"};
+      auto unknowns = system.solve().value();
+
+      // Fill base node potential with zero.
+      auto result_potentials = solution_potentials{};
+      for (const auto &v : network.m_graph) {
+        const auto id = v.first;
+        const auto mapped = id_map.at(id);
+        result_potentials[id] = unknowns[mapped][0];
+      }
+
+      // Fill unkown currents that were found as a part of linear system of m_equations.
+      auto result_currents = solution_currents{};
+
+      for (const auto &v : short_circuit_current_map) {
+        const auto fwd_current = unknowns[v.second.first][0];
+        const auto first_original_id = inverse_id_map.at(v.first.first);
+        const auto second_original_id = inverse_id_map.at(v.first.second);
+        result_currents[first_original_id][second_original_id] = fwd_current;
+        result_currents[second_original_id][first_original_id] = -fwd_current;
+      }
+
+      // Compute other currents from potentials, when there are no short-circuits.
+      for (const auto &v : network.m_graph) {
+        for (const auto &c : v.second) {
+          const auto first_id = v.first, second_id = c.first;
+          if (throttle::is_roughly_equal(c.second.first, 0.0)) continue;
+          const auto [res, emf] = c.second;
+          const auto fwd_current = (result_potentials[first_id] - result_potentials[second_id] + emf) / res;
+          result_currents[first_id][second_id] = fwd_current;
+        }
+      }
+
+      return solution{result_potentials, result_currents};
     }
+  };
 
-    // Compute other currents from potentials, when there are no short-circuits.
+  friend connected_resistor_network_solver;
+
+public:
+  // The graph passed to this constructor must consist of only one connected components. This requirement is not
+  // validated in any way whatsoever. This class is not supposed to be used by the end user.
+  connected_resistor_network(circuit_graph_type graph) : m_graph{graph} {
     for (const auto &v : m_graph) {
-      for (const auto &c : v.second) {
-        const auto first_id = v.first, second_id = c.first;
-        if (throttle::is_roughly_equal(c.second.first, 0.0)) continue;
-        const auto [res, emf] = c.second;
-        const auto fwd_current = (result_potentials[first_id] - result_potentials[second_id] + emf) / res;
-        result_currents[first_id][second_id] = fwd_current;
+      for (const auto &p : v.second) {
+        auto first = v.first, second = p.first;
+        auto [res, emf] = p.second;
+        if (throttle::is_roughly_equal(res, 0.0)) m_short_circuits.push_back({first, second, emf});
       }
     }
+  }
 
-    return {result_potentials, result_currents};
+  circuit_graph_type graph() const { return m_graph; }
+
+  solution solve() const {
+    connected_resistor_network_solver solver{*this};
+    return solver.solve();
   }
 };
 } // namespace detail
